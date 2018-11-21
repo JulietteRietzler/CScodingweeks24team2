db9937b95a0fda458e09b5e0f87a5a8fdb9d6de4
JulietteRietzler <juliette.rietzler@student-cs.fr>
2018-11-21 10:30:06 +0100

    minor update

--- MVP3/récupération_test.py
+++ MVP3/récupération_test.py
@@ -1,18 +1,22 @@
 
 def récupérationt(fichier):
+    '''
+    :param fichier: le fichier à analyser
+    :return: une liste des tests contenus dans le fichier.
+    '''
     with open(fichier,'r')as code:
         codelignes=code.readlines()
-        liste_test=[]
+        liste_tests=[]
         index=0
         while index<len(codelignes):
             if "test" in codelignes[index] and "do\n" in codelignes[index] :
                 compteur=index+1
                 while 'end\n' not in codelignes[compteur]:
                     compteur+=1
-                liste_test.append(codelignes[index:compteur+1])
+                liste_tests.append(codelignes[index:compteur+1])
                 index=compteur
             else:
                 index+=1
-        return(liste_test)
+        return(liste_tests)
 
 print(récupérationt('EventCandidatATest.rb'))
